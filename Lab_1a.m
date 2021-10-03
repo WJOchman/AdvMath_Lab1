f9b8b481315cda036faea8ae39125d41e1ac3256
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-10-04 00:00:46 +0100

    Uploaded Lab1 Task1

---
+++ Lab_1a.m
@@ -0,0 +1,28 @@
+clear % clears the memory % 
+clc % clears the screen%
+
+% Task 1: Construction of continuous time signal %
+
+start = 0;
+increment = 0.001;
+stop = 0.666;
+t = start : increment : stop 
+
+x_c = sin(2*pi*3*t)
+plot (t, x_c)
+
+% Task 2: Plotting of continous time signal %
+
+
+
+% Task 3: Continuous time to discrete converstion %
+
+
+
+% Task 4: Displaying Results %
+
+
+
+% Task 5: Data Handling %
+
+
