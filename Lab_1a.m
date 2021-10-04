ce4525e0050d080b4953eb163af7c7369aad0d79
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-10-04 10:50:44 +0100

    Updated jobs for Task 2

--- Lab_1a.m
+++ Lab_1a.m
@@ -6,14 +6,19 @@ clc % clears the screen%
 start = 0;
 increment = 0.001;
 stop = 0.666;
-t = start : increment : stop 
+t = start : increment : stop; 
 
-x_c = sin(2*pi*3*t)
-plot (t, x_c)
+x_c = sin(2*pi*3*t);
+plot (t, x_c);
 
 % Task 2: Plotting of continous time signal %
 
-
+% time vs magnitude %
+% the trace must have a line of 2 %
+% A main title which ust be 16 font, bold, and color black %
+% The tick marks must be 14 font, bold %
+% The y-axis must range from -1.1 to +1.1 %
+% The plot must contain a grid %
 
 % Task 3: Continuous time to discrete converstion %
 
@@ -24,5 +29,3 @@ plot (t, x_c)
 
 
 % Task 5: Data Handling %
-
-
